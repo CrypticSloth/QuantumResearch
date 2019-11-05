0ec315861332b4ea23a42b76549c4aa50871120c
Erik Sorensen <sorensen.erik48@gmail.com>
2019-11-05 18:26:36 -0500

    Quantum runs, can only run with 4 stocks or 4 qumodes else it has a memory error. After softmax is applied it calls for buying stocks evently. Maybe this is a problem...

--- NES_Meta_Trading/updated-NES-google-deterministic.py
+++ NES_Meta_Trading/updated-NES-google-deterministic.py
@@ -12,7 +12,7 @@ import pandas as pd
 sns.set()
 
 import os
-os.chdir("D:/Github/QuantumResearch/NES_Meta_Trading/")
+os.chdir("C:/Github/QuantumResearch/NES_Meta_Trading/")
 
 # In[58]:
 
@@ -153,66 +153,312 @@ class Model:
     def set_weights(self, weights):
         self.weights = weights
 
-# %%
+# %% ###########################################################################
+# Quantum Model
 
 class QuantumModel:
-    def __init__(self, num_layers):
-        self.weights = 0.05 * np.random.randn(num_layers, 5)
+    def __init__(self, num_layers, output_size, bs):
+        self.num_layers = num_layers
+        self.output_size = output_size
+        self.bs = bs
+        self.weights = 0.05 * np.random.randn(self.num_layers, 5 * 5)
 
-    dev = qml.device("strawberryfields.fock", wires=1, cutoff_dim=10)
+    # NOTE: 5 wires gives a memory error so 4 seems to be the max
+    dev = qml.device("strawberryfields.fock", wires=output_size, cutoff_dim=10)
 
-    def layer(self, v):
-        '''
-            For each weight (v[i]) apply the quantum gates to it.
+    if bs == False:
+        def layer(self, w):
+            '''
+                For each weight (w[i]) apply the quantum gates to it.
 
-            Saves the updated weights to the quantum device.
+                Saves the updated weights to the quantum device.
 
-            v: a list of scalar weights (of length 5)
-        '''
-        # Matrix multiplication of input layer
-        qml.Rotation(v[0], wires=0)
-        qml.Squeezing(v[1], 0.0, wires=0)
-        qml.Rotation(v[2], wires=0)
+                w: a list of scalar weights (of length 5)
+            '''
 
-        # Bias
-        qml.Displacement(v[3], 0.0, wires=0)
+            # Matrix multiplication of input layer
+            qml.Rotation(w[0], wires=0)
+            qml.Rotation(w[1], wires=1)
+            qml.Rotation(w[2], wires=2)
+            qml.Rotation(w[3], wires=3)
+            # qml.Rotation(w[4], wires=4)
 
-        # Element-wise nonlinear transformation
-        qml.Kerr(v[4], wires=0)
+            qml.Squeezing(w[5], 0.0, wires=0)
+            qml.Squeezing(w[6], 0.0, wires=1)
+            qml.Squeezing(w[7], 0.0, wires=2)
+            qml.Squeezing(w[8], 0.0, wires=3)
+            # qml.Squeezing(w[9], 0.0, wires=4)
 
-    @qml.qnode(dev)
-    def quantum_neural_net(self, var, x=None):
-        '''
-            For each layer, apply the inputs to the gates to update the weights
+            qml.Rotation(w[10], wires=0)
+            qml.Rotation(w[11], wires=1)
+            qml.Rotation(w[12], wires=2)
+            qml.Rotation(w[13], wires=3)
+            # qml.Rotation(w[14], wires=4)
 
-            var: list of lists of scalar weights (of length 5)
-            x: scalar value, the input training data
-        '''
-        # Encode input x into quantum state
-        qml.Displacement(x, 0.0, wires=0)
+            # Bias
+            qml.Displacement(w[15], 0.0, wires=0)
+            qml.Displacement(w[16], 0.0, wires=1)
+            qml.Displacement(w[17], 0.0, wires=2)
+            qml.Displacement(w[18], 0.0, wires=3)
+            # qml.Displacement(w[19], 0.0, wires=4)
 
-        # "layer" subcircuits
-        for v in var:
-            self.layer(v)
+            # Element-wise nonlinear transformation
+            qml.Kerr(w[20], wires=0)
+            qml.Kerr(w[21], wires=1)
+            qml.Kerr(w[22], wires=2)
+            qml.Kerr(w[23], wires=3)
+            # qml.Kerr(w[24], wires=4)
 
-        return qml.expval(qml.X(0))
+        @qml.qnode(dev)
+        def quantum_neural_net(self, weights, x=None):
+            '''
+                For each layer, apply the inputs to the gates to update the weights
 
-    def predict(self, inputs):
-        '''
-            Loop through each of the training data and apply it to the quantum network to get a prediction for each value.
+                weights: list of lists of scalar weights (of length 5)
+                x: list of stock closing values for 5 stocks
+            '''
+            # Encode input x into quantum state
+            qml.Displacement(x[0], 0.0, wires=0)
+            qml.Displacement(x[1], 0.0, wires=1)
+            qml.Displacement(x[2], 0.0, wires=2)
+            qml.Displacement(x[3], 0.0, wires=3)
+            # qml.Displacement(x[4], 0.0, wires=4)
 
-            Will need to somehow make the QNN shape the values to output 5 values for the softmax function. Not sure how to do this since the network only updates with scalar values and the output is the size of the number of inputs. 
-        '''
-        preds = [self.quantum_neural_net(self.weights, x=x) for x in inputs]
-        return preds
+            # "layer" subcircuits
+            for w in weights:
+                self.layer(w)
 
-    def get_weights(self):
-        return self.weights
+            return [qml.expval(qml.X(0)),
+                    qml.expval(qml.X(1)),
+                    qml.expval(qml.X(2)),
+                    qml.expval(qml.X(3))]
+                    # qml.expval(qml.X(4))]
 
-    def set_weights(self, weights):
-        self.weights = weights
+        def predict(self, weights, inputs):
+            '''
+                Loop through each of the training data and apply it to the quantum network to get a prediction for each value.
 
-# In[66]:
+                Will need to somehow make the QNN shape the values to output 5 values for the softmax function. Not sure how to do this since the network only updates with scalar values and the output is the size of the number of inputs.
+            '''
+            preds = np.array([self.quantum_neural_net(self.weights, x=x) for x in inputs.T])
+
+            return [np.mean(p) for p in preds.T]
+
+    if bs == True:
+
+        def layer(self, w):
+            '''
+                For each weight (w[i]) apply the quantum gates to it.
+
+                Saves the updated weights to the quantum device.
+
+                w: a list of scalar weights (of length 5)
+            '''
+
+            qml.Displacement(w[0], 0.0, wires=0)
+            qml.Displacement(w[1], 0.0, wires=1)
+            qml.Displacement(w[2], 0.0, wires=2)
+            qml.Displacement(w[3], 0.0, wires=3)
+
+            qml.Displacement(w[4], w[5], wires=0)
+            qml.Displacement(w[6], w[7], wires=1)
+            qml.Displacement(w[8], w[9], wires=2)
+            qml.Displacement(w[10], w[11], wires=3)
+
+            qml.Squeezing(w[12], w[13], wires=0)
+            qml.Squeezing(w[14], w[15], wires=1)
+            qml.Squeezing(w[16], w[17], wires=2)
+            qml.Squeezing(w[18], w[19], wires=3)
+
+            qml.Kerr(w[20], wires=0)
+            qml.Kerr(w[21], wires=1)
+            qml.Kerr(w[22], wires=2)
+            qml.Kerr(w[23], wires=3)
+
+            qml.Beamsplitter(w[24],0, wires=[0,1])
+            qml.Beamsplitter(0,0, wires=[0,1])
+            qml.Beamsplitter(w[25],0, wires=[1,2])
+            qml.Beamsplitter(0,0, wires=[1,2])
+            qml.Beamsplitter(w[26],0, wires=[2,3])
+            qml.Beamsplitter(0,0, wires=[2,3])
+
+            qml.Displacement(w[27], w[28], wires=0)
+            qml.Displacement(w[29], w[30], wires=1)
+            qml.Displacement(w[31], w[32], wires=2)
+            qml.Displacement(w[33], w[34], wires=3)
+
+            qml.Squeezing(w[35], w[36], wires=0)
+            qml.Squeezing(w[37], w[38], wires=1)
+            qml.Squeezing(w[39], w[40], wires=2)
+            qml.Squeezing(w[41], w[42], wires=3)
+
+            qml.Kerr(w[43], wires=0)
+            qml.Kerr(w[44], wires=1)
+            qml.Kerr(w[45], wires=2)
+            qml.Kerr(w[46], wires=3)
+
+            qml.Beamsplitter(w[47],0, wires=[0,1])
+            qml.Beamsplitter(0,0, wires=[0,1])
+            qml.Beamsplitter(w[48],0, wires=[1,2])
+            qml.Beamsplitter(0,0, wires=[1,2])
+            qml.Beamsplitter(w[49],0, wires=[2,3])
+            qml.Beamsplitter(0,0, wires=[2,3])
+
+
+        @qml.qnode(dev)
+        def quantum_neural_net(self, weights, x=None):
+            '''
+                For each layer, apply the inputs to the gates to update the weights
+
+                weights: list of lists of scalar weights (of length 5)
+                x: list of stock closing values for 5 stocks
+            '''
+            # Encode input x into quantum state
+            qml.Displacement(x[0], 0.0, wires=0)
+            qml.Displacement(x[1], 0.0, wires=1)
+            qml.Displacement(x[2], 0.0, wires=2)
+            qml.Displacement(x[3], 0.0, wires=3)
+            # qml.Displacement(x[4], 0.0, wires=4)
+
+            # "layer" subcircuits
+            for w in weights:
+                self.layer(w)
+
+            return [qml.expval(qml.X(0)),
+                    qml.expval(qml.X(1)),
+                    qml.expval(qml.X(2)),
+                    qml.expval(qml.X(3))]
+                    # qml.expval(qml.X(4))]
+
+        def predict(self, weights, inputs):
+            '''
+                Loop through each of the training data and apply it to the quantum network to get a prediction for each value.
+
+                Will need to somehow make the QNN shape the values to output 5 values for the softmax function. Not sure how to do this since the network only updates with scalar values and the output is the size of the number of inputs.
+            '''
+            preds = np.array([self.quantum_neural_net(self.weights, x=x) for x in inputs.T])
+
+            return [np.mean(p) for p in preds.T]
+
+
+# num_layers = 4 # 4-6 number of layers
+# output_size = 4 # Number of stocks
+# weights = 0.05 * np.random.randn(num_layers, 5 * 5) # the 5*5 just has to accomidate the number of stocks * number of gates we will have
+# np.shape(weights)
+#
+# weights
+#
+# # for layer in weights:
+# #     for w in layer:
+# #         print(w)
+#
+# # NOTE: 5 wires gives a memory error so 4 seems to be the max
+# dev = qml.device("strawberryfields.fock", wires=output_size, cutoff_dim=10)
+#
+# def layer(w):
+#     '''
+#         For each weight (w[i]) apply the quantum gates to it.
+#
+#         Saves the updated weights to the quantum device.
+#
+#         w: a list of scalar weights (of length 5)
+#     '''
+#     print()
+#     # Matrix multiplication of input layer
+#     qml.Rotation(w[0], wires=0)
+#     qml.Rotation(w[1], wires=1)
+#     qml.Rotation(w[2], wires=2)
+#     qml.Rotation(w[3], wires=3)
+#     # qml.Rotation(w[4], wires=4)
+#
+#     qml.Squeezing(w[5], 0.0, wires=0)
+#     qml.Squeezing(w[6], 0.0, wires=1)
+#     qml.Squeezing(w[7], 0.0, wires=2)
+#     qml.Squeezing(w[8], 0.0, wires=3)
+#     # qml.Squeezing(w[9], 0.0, wires=4)
+#
+#     qml.Rotation(w[10], wires=0)
+#     qml.Rotation(w[11], wires=1)
+#     qml.Rotation(w[12], wires=2)
+#     qml.Rotation(w[13], wires=3)
+#     # qml.Rotation(w[14], wires=4)
+#
+#     # Bias
+#     qml.Displacement(w[15], 0.0, wires=0)
+#     qml.Displacement(w[16], 0.0, wires=1)
+#     qml.Displacement(w[17], 0.0, wires=2)
+#     qml.Displacement(w[18], 0.0, wires=3)
+#     # qml.Displacement(w[19], 0.0, wires=4)
+#
+#     # Element-wise nonlinear transformation
+#     qml.Kerr(w[20], wires=0)
+#     qml.Kerr(w[21], wires=1)
+#     qml.Kerr(w[22], wires=2)
+#     qml.Kerr(w[23], wires=3)
+#     # qml.Kerr(w[24], wires=4)
+#
+# @qml.qnode(dev)
+# def quantum_neural_net(weights, x=None):
+#     '''
+#         For each layer, apply the inputs to the gates to update the weights
+#
+#         weights: list of lists of scalar weights (of length 5)
+#         x: list of stock closing values for 5 stocks
+#     '''
+#     # Encode input x into quantum state
+#     qml.Displacement(x[0], 0.0, wires=0)
+#     qml.Displacement(x[1], 0.0, wires=1)
+#     qml.Displacement(x[2], 0.0, wires=2)
+#     qml.Displacement(x[3], 0.0, wires=3)
+#     # qml.Displacement(x[4], 0.0, wires=4)
+#
+#     # "layer" subcircuits
+#     for w in weights:
+#         layer(w)
+#
+#     return [qml.expval(qml.X(0)),
+#             qml.expval(qml.X(1)),
+#             qml.expval(qml.X(2)),
+#             qml.expval(qml.X(3))]
+#             # qml.expval(qml.X(4))]
+#
+# def predict(weights, inputs):
+#     '''
+#         Loop through each of the training data and apply it to the quantum network to get a prediction for each value.
+#
+#         Will need to somehow make the QNN shape the values to output 5 values for the softmax function. Not sure how to do this since the network only updates with scalar values and the output is the size of the number of inputs.
+#     '''
+#     preds = np.array([quantum_neural_net(weights, x=x) for x in inputs.T])
+#
+#     return [np.mean(p) for p in preds.T]
+
+# def get_weights(self):
+#     return self.weights
+#
+# def set_weights(self, weights):
+#     self.weights = weights
+
+num_days = 30
+close, names = load_data("dataset/train/",num_days)
+num_stocks = len(names) # This will need to be used to calculate the iterations and input layer sizes along with num_days
+num_stocks
+np.shape(close)
+
+model = QuantumModel(num_layers = 4, output_size = num_stocks)
+
+cur_state = get_state(close, 10, window_size + 1, num_days, num_stocks)
+# act(model, np.array([0.,0.,0.,0.]))
+
+cur_state = cur_state.reshape(num_stocks, window_size)
+np.shape(cur_state)
+
+dev.reset()
+preds = model.predict(weights, cur_state) # This is all we need..
+preds
+
+# softmax(np.array([preds]))
+
+# In[66]: ######################################################################
 
 def softmax(x):
     """Compute softmax values for each sets of scores in x."""
@@ -262,26 +508,24 @@ def stock_value(inventory, money, close_s, t):
     cash = np.sum([close_s[i][t] * inventory[i] for i in range(len(close_s))]) + money
     return cash
 
-
 # Testing one iteration of the new reward function
 # This assumes we can purchase partial stocks and has no limits
-window_size = 9
-# model = Model(window_size*num_stocks, 500, 3)
-model = QuantumModel(num_layers=4)
 
-cur_state = get_state(close, 0, window_size + 1, num_days, num_stocks)
+num_days = 30
+close, names = load_data("dataset/train/",num_days)
+num_stocks = len(names) # This will need to be used to calculate the iterations and input layer sizes along with num_days
+num_stocks
 
-np.shape(cur_state)
-np.array(cur_state[0])
-act(model, np.array([0.,0.,0.,0.]))
+window_size = 10
 
-model.predict([0,1,2,3,4])
+model = Model(window_size*num_stocks, 500, 3)
+# model = QuantumModel(num_layers=4)
 
 weight = model
 initial_money = 10000
 starting_money = initial_money
 
-# cur_state = get_state(close, 0, window_size + 1).reshape(num_stocks,window_size)
+cur_state = get_state(close, 0, window_size + 1, num_days, num_stocks)
 close_s = close.reshape(num_stocks,int(len(close)/num_stocks))
 skip = 1
 
