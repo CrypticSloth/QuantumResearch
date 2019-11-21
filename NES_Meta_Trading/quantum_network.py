1b60f70531a9df4e5a0741abd30f7aefcd62da91
CrypticSl0th <sorensen.erik48@gmail.com>
2019-11-20 21:42:37 -0500

    window size of 1 while exploring different limits might be a good idea.

--- NES_Meta_Trading/quantum_network.py
+++ NES_Meta_Trading/quantum_network.py
@@ -127,7 +127,7 @@ def quantum_neural_net(weights, x=None, bs=False):
         weights: list of lists of scalar weights (of length 5)
         x: list of stock closing values for 5 stocks
     '''
-    print(x)
+    # print("X: ", x)
     # Encode input x into quantum state
     qml.Displacement(x[0], 0.0, wires=0)
     qml.Displacement(x[1], 0.0, wires=1)
@@ -159,138 +159,144 @@ def predict(inputs, weights, bs=False):
             for y in np.array(inputs).T
             for x in [y]]
     # preds = np.array([quantum_neural_net(weights, x=x ,bs=bs) for x in inputs.T])
-
+    # print("P: ", preds)
+    # print("Ps: ", [np.sum(p) for p in np.array(preds).T] )
     return [np.sum(p) for p in np.array(preds).T] # I feel that this could be wrong
 
 # %%
 # Testing code
-
-def buy_stock(portfolio, close_s, money, inventory, limit, t):
-    """
-        Function that takes in portfolio weights (percentage of each stock in the entire portfolio),
-        the current stock prices (close price) and the money we currently have
-        and calculates the maximum number of stocks we can buy with the weights given in the portfolio.
-
-        Inventory is the dictionary containing how many stocks we own.
-        Limit puts a maximum number of stock we can purchase
-        t is the current time step
-
-        TODO: Getting negetive inventory values from negetive cash
-        TODO: instead of dealing with cash amounts we should deal with percentage gain (normalized)?
-    """
-
-    c = 0
-    cash = np.sum([close_s[i][t] * inventory[i] for i in range(len(close_s))]) + money # reset our inventory into cash
-
-    portfolio_money = portfolio[0] * cash
-
-    p = []
-    for m in portfolio_money:
-        num_stock = math.floor(m / (close_s[c][t] + 0.000001))
-        p.append(close_s[c][t])
-        if num_stock <= limit:
-            inventory[c] = num_stock
-        else:
-            inventory[c] = limit
-
-        cash -= (inventory[c] * close_s[c][t])
-        c += 1
-
-    return inventory, cash
-
-import os
-import numpy as np
-import math
-os.chdir("D:/Github/QuantumResearch/NES_Meta_Trading/")
-
-from updated_NES_google_deterministic import load_data, get_state
-import warnings
-warnings.filterwarnings('ignore')
-
-def test():
-    num_days = 30
-    close, names = load_data("dataset/train_q/",num_days)
-    num_stocks = len(names) # This will need to be used to calculate the iterations and input layer sizes along with num_days
-    num_stocks
-    np.shape(close)
-
-    def softmax(x):
-        """Compute softmax values for each sets of scores in x."""
-        e_x = np.exp(x - np.max(x))
-        return e_x / (e_x.sum(axis=1) + 0.00001)
-
-    def act(sequence, weights):
-        decision = predict(np.array(sequence).reshape(num_stocks,window_size), weights)
-        # print(decision)
-        # print(self.softmax([decision]) * 100)
-        return softmax(np.array([decision]) * 10)
-
-    num_layers = 4
-    weights = 0.05 * np.random.randn(num_layers, 10)
-
-    initial_money = 10000
-    window_size = 10
-    limit = 5
-    starting_money = initial_money
-    close_s = close.reshape(num_stocks,int(len(close)/num_stocks))
-
-    close = close_s.flatten() # Use the split data for close
-    np.shape(close)
-
-    # Initialize a dictionary to keep track of which stocks we can buy
-    keys = range(num_stocks)
-    cur_inventory = {key: 0 for key in keys}
-
-
-    cur_state = get_state(close, 0, window_size + 1, num_days, num_stocks)
-    np.shape(cur_state)
-    for t in range(0, len(close_s[0]) - 1):
-
-        portfolio = act(cur_state, weights)
-        print(portfolio)
-        next_state = get_state(close, t + 1, window_size + 1, num_days, num_stocks)
-        next_inventory, initial_money = buy_stock(portfolio, close_s, initial_money, cur_inventory, limit, t)
-
-        cur_state = next_state
-        cur_inventory = next_inventory
-
-    return cur_inventory, (initial_money / starting_money - 1) * 100 # rate of returns
-
-# close
-cur_state = get_state(close, 10, window_size + 1, num_days, num_stocks)
+#
+# def buy_stock(portfolio, close_s, money, inventory, limit, t):
+#     """
+#         Function that takes in portfolio weights (percentage of each stock in the entire portfolio),
+#         the current stock prices (close price) and the money we currently have
+#         and calculates the maximum number of stocks we can buy with the weights given in the portfolio.
+#
+#         Inventory is the dictionary containing how many stocks we own.
+#         Limit puts a maximum number of stock we can purchase
+#         t is the current time step
+#
+#         TODO: Getting negetive inventory values from negetive cash
+#         TODO: instead of dealing with cash amounts we should deal with percentage gain (normalized)?
+#     """
+#
+#     c = 0
+#     cash = np.sum([close_s[i][t] * inventory[i] for i in range(len(close_s))]) + money # reset our inventory into cash
+#
+#     portfolio_money = portfolio[0] * cash
+#
+#     p = []
+#     for m in portfolio_money:
+#         num_stock = math.floor(m / (close_s[c][t] + 0.000001))
+#         p.append(close_s[c][t])
+#         if num_stock <= limit:
+#             inventory[c] = num_stock
+#         else:
+#             inventory[c] = limit
+#
+#         cash -= (inventory[c] * close_s[c][t])
+#         c += 1
+#
+#     return inventory, cash
+#
+# import os
+# import numpy as np
+# import math
+# os.chdir("D:/Github/QuantumResearch/NES_Meta_Trading/")
+#
+# from updated_NES_google_deterministic import load_data, get_state
+# import warnings
+# warnings.filterwarnings('ignore')
+#
+# def test():
+#     num_days = 30
+#     close, names = load_data("dataset/train_q/",num_days)
+#     num_stocks = len(names) # This will need to be used to calculate the iterations and input layer sizes along with num_days
+#     num_stocks
+#     np.shape(close)
+#
+#     def softmax(x):
+#         """Compute softmax values for each sets of scores in x."""
+#         e_x = np.exp(x - np.max(x))
+#         return e_x / (e_x.sum(axis=1) + 0.00001)
+#
+#     def act(sequence, weights):
+#         decision = predict(np.array(sequence).reshape(num_stocks,window_size), weights)
+#         # print(decision)
+#         # print(self.softmax([decision]) * 100)
+#         return softmax(np.array([decision]) * 1)
+#
+#     num_layers = 4
+#     weights = 0.05 * np.random.randn(num_layers, 10)
+#
+#     initial_money = 10000
+#     window_size = 1
+#     limit = math.inf
+#     starting_money = initial_money
+#     close_s = close.reshape(num_stocks,int(len(close)/num_stocks))
+#
+#     close = close_s.flatten() # Use the split data for close
+#     np.shape(close)
+#
+#     # Initialize a dictionary to keep track of which stocks we can buy
+#     keys = range(num_stocks)
+#     cur_inventory = {key: 0 for key in keys}
+#
+#
+#     cur_state = get_state(close, 0, window_size + 1, num_days, num_stocks)
+#     np.shape(cur_state)
+#     for t in range(0, len(close_s[0]) - 1):
+#
+#         portfolio = act(cur_state, weights)
+#         print("S: ", portfolio)
+#         next_state = get_state(close, t + 1, window_size + 1, num_days, num_stocks)
+#         next_inventory, initial_money = buy_stock(portfolio, close_s, initial_money, cur_inventory, limit, t)
+#
+#         cur_state = next_state
+#         print("I :", next_inventory)
+#         cur_inventory = next_inventory
+#
+#     return cur_inventory, (initial_money / starting_money - 1) * 100 # rate of returns
+#
+# test()
+#
+# # close
+# cur_state = get_state(close, 1, window_size + 1, num_days, num_stocks)
+# cur_state
+# # cur_state.reshape(num_stocks, window_size)
+# bs = False
+# preds = [quantum_neural_net(weights, x=x, bs=bs)
+#         for y in np.array(cur_state).reshape(num_stocks, window_size).T
+#         for x in [y]]
+# [x
+# for y in np.array(cur_state).reshape(num_stocks, window_size).T
+# for x in [y]]
+#
 # cur_state.reshape(num_stocks, window_size)
-bs = False
-preds = [quantum_neural_net(weights, x=x, bs=bs)
-        for y in np.array(cur_state).reshape(num_stocks, window_size).T
-        for x in [y]]
-[x
-for y in np.array(cur_state).reshape(num_stocks, window_size).T
-for x in [y]]
+# np.array(preds).T
+#
+# [np.sum(p) for p in np.array(preds).T]
+#
+# port = act(cur_state, weights)
+# port
+#
+# test()
+#
+# x_pred = np.array([np.linspace(-1,1,10), np.linspace(-1,1,10)])
+# predictions = [quantum_neural_net(weights, x=x_) for x_ in x_pred.T]
+# predictions
+# t1 = []
+# t2 = []
+# for i in range(30):
+#     inv, rew = test()
+#     t2.append(rew)
+#     t1.append(inv)
+# t1
+# t2
 
-cur_state.reshape(num_stocks, window_size)
-np.array(preds).T
-
-[np.sum(p) for p in np.array(preds).T]
-
-port = act(cur_state, weights)
-port
-
-test()
-
-x_pred = np.array([np.linspace(-1,1,10), np.linspace(-1,1,10)])
-predictions = [quantum_neural_net(weights, x=x_) for x_ in x_pred.T]
-predictions
-t1 = []
-t2 = []
-for i in range(10):
-    inv, rew = test()
-    t2.append(rew)
-    t1.append(inv)
-t1
-t2
-
-test()
-test()
+# test()
+# test()
+#
 
 # %%
 
@@ -581,7 +587,7 @@ if __name__ == '__main__':
     num_stocks
     np.shape(close)
 
-    window_size = 10
+    window_size = 1
     cur_state = get_state(close, 10, window_size + 1, num_days, num_stocks)
 
     # %%
@@ -599,7 +605,7 @@ if __name__ == '__main__':
     # model = Model(input_size = window_size*num_stocks, layer_size = 500, output_size = len(names))
     agent = Agent(
         money = 10000,
-        limit = 5,
+        limit = 20,
         close = close,
         window_size = window_size,
         num_stocks = len(names),
@@ -611,7 +617,7 @@ if __name__ == '__main__':
 
     # In[79]:
 
-    agent.fit(iterations = 5, checkpoint = 1)
+    agent.fit(iterations = 500, checkpoint = 10)
 
     # Weights are changing...
     # print(agent.weights)
